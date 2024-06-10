88778d2bdcbcad33be09d3f19519a6e883de9f41
admin <admin@Kostadins-MacBook-Pro.local>
2024-06-10 03:03:41 +0300

    Added fix

--- components/sheet/SheetItems.vue
+++ components/sheet/SheetItems.vue
@@ -4,10 +4,10 @@
 			<div
 			v-if="item.length > 1"
 			
-				class="flex justify-between items-center font-bg text-white my-2"
+				class="flex justify-between items-center font-bg text-white my-2 "
 			>
 				<div>
-					<p class="text-4xl mr-2 text-left leading-7">
+					<p class="text-3xl text-left leading-7 m-2 ">
 						{{ item[0] || "-" }}
 					</p>
 				
@@ -15,7 +15,7 @@
 	
 				<div class="flex flex-col justify-center items-center">
 					<div v-for="indexes in 10" :key="indexes">
-						<span class="text-2xl text-left leading-7">
+						<span class="lg:text-2xl sm:text-xl text-left leading-7">
 						{{ item[indexes]  }}
 						</span>
 						<span v-if="item.length > 2 && item[indexes+1]" class=" mr-2">,</span>
